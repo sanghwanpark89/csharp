82f2a9d4cc86d2a09cb7579dfc781b79998d0cba
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-02-17 17:24:18 -0500

    Create CustomerDateOfBirth.cs

---
+++ ConstructorAndStruct/CustomerDateOfBirth.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickKartBL
+{
+    public struct CustomerDateOfBirth
+    {
+        public int custDoBDate;
+        public int custDoBMonth;
+        public int custDoBYear;
+    }
+}
