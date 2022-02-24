418bb7beff8b559649e9d0350a3005695c8ef623
sanghwanpark89 <81578411+sanghwanpark89@users.noreply.github.com>
2022-02-24 11:02:21 -0500

    Create Product.cs

---
+++ Collections/ArrayList/Product.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace QuickKartBL
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public double Price { get; set; }
+
+        public int QuantityAvailable { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public static int counter;
+
+        static Product()
+        {
+            counter = 1000;
+        }
+
+        public Product()
+        {
+            ProductId = "P" + (++counter);
+        }
+
+        public Product(string productName, double price, int quantityAvailable, int categoryId): this()
+        {
+            ProductName = productName;
+            Price = price;
+            QuantityAvailable = quantityAvailable;
+            CategoryId = categoryId;
+        }
+
+
+
+    }
+}
